56595bf9e9897a10877bdd4d0ecb368895ecdf29
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-19 11:27:45 -0400

    dark mode localstorage, adding new page files

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,18 @@
 const darkModeButton = document.getElementById("dark-mode-toggle")
 const cartCount = document.getElementById("cart-item-count")
 
+
+if(localStorage.getItem("mode") === "Dark Mode") {
+    document.body.classList.add("dark-mode")
+    darkModeButton.textContent = "Light Mode"
+} else {
+    document.body.classList.remove("dark-mode")
+    darkModeButton.textContent = "Dark Mode"
+}
+
 darkModeButton.addEventListener('click', () => {
     document.body.classList.toggle("dark-mode")
+    localStorage.setItem("mode", darkModeButton.textContent)
     darkModeButton.textContent = (darkModeButton.textContent == "Dark Mode") ? "Light Mode" : "Dark Mode"
 })
 
