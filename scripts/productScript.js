f7322b7992c0c89714b27b4f3ac80b646ca85c94
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-19 23:54:24 -0400

    added favicon and logo, fixed page titles

--- scripts/productScript.js
+++ scripts/productScript.js
@@ -1,5 +1,14 @@
 const termDisplay = document.getElementById("search-term")
+const individualProduct = document.getElementById("individual-product-container")
 
 let qParams = new URLSearchParams(window.location.search)
 
-termDisplay.textContent += qParams.get("searchbar")
+if(termDisplay) {
+    termDisplay.textContent += qParams.get("searchbar")
+    document.title += " " +  qParams.get("searchbar")
+    console.log(pageTitle)
+}
+
+if(individualProduct) {
+    document.title += " " + document.getElementsByClassName("product-title")[0].textContent
+}
