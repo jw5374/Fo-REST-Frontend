49f6a1b2157fb6ac0341ae025f90fc9cd41d2968
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-21 18:00:41 -0400

    adding product image fetching

--- scripts/allProducts.js
+++ scripts/allProducts.js
@@ -1,6 +1,8 @@
 const productGallery = document.getElementById("product-gallery")
 
 function createProdContainer(prodObj) {
+    let imgList = prodObj.imageList.split(',')
+
     let linkContainer = document.createElement("a")
     linkContainer.classList.add("product-link")
     linkContainer.href = `product.html?item=${prodObj.id}`
@@ -10,7 +12,7 @@ function createProdContainer(prodObj) {
 
     let prodImg = document.createElement("img")
     prodImg.classList.add("product-image")
-    prodImg.src = "#"
+    prodImg.src = "../assets/GundamGallery/" + imgList[imgList.length - 1]
     prodImg.alt = "product image"
 
     let descContainer = document.createElement("article")
