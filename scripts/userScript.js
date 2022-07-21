49e42080fda192dac8fee7c4070144272158198a
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-21 14:36:19 -0400

    attempting to fix hash func

--- scripts/userScript.js
+++ scripts/userScript.js
@@ -1,23 +1,8 @@
 const registrationForm = document.getElementById("user-form")
 
-
-// mdn SubtleCrypto docs
-async function digestMessage(message) {
-    const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
-    const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
-    return hashHex;
-}
-
-async function getDigested(message) {
-    const data = await digestMessage(message);
-    return data;
-}
-
 registrationForm.addEventListener("submit", async (e) => {
     e.preventDefault()
-    let hashedWord = await getDigested(registrationForm.passinput.value)
+    let hashedWord = hex_sha256(registrationForm.passinput.value)
     fetch(fetchPath + "/register", {
         method: "POST",
         headers: {
