e7dca16520fbc9148aa492bf66ac13e326396a2c
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-22 20:32:19 -0400

    implementing logged in status checking

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,47 @@ const darkModeButton = document.getElementById("dark-mode-toggle")
 const cartCount = document.getElementById("cart-item-count")
 const searchButton = document.querySelector(".bi.bi-search")
 const searchBar = document.getElementById("search-bar")
+const profile = document.querySelector(".bi.bi-person")
 
+function getAuthCookie() {
+    for(let cookie of document.cookie.split(";")) {
+        let cookieEntry = cookie.split("=")
+        if(cookieEntry[0] !== "token") {
+            continue;
+        }
+        return cookieEntry[1]
+    }
+    return null;
+}
+
+function checkCookie() {
+    let auth = getAuthCookie()
+    if(auth == null) {
+        return;
+    }
+    fetch(fetchPath + "/auth/user", {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${auth}`
+        }
+    })
+    .then(res => {
+        if(res.status != 200) {
+            document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/"
+            localStorage.removeItem("forest-user")
+            return;
+        }
+        return res.text()
+    })
+    .then(data => {
+        if(data.split(" ")[0] != localStorage.getItem("forest-user")) {
+            document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/"
+            localStorage.removeItem("forest-user")
+            return;
+        }
+        profile.parentElement.href = window.location.protocol + "//" + window.location.host + "/profile.html"
+    })
+}
 
 if(localStorage.getItem("mode") === "Dark Mode") {
     document.body.classList.add("dark-mode")
@@ -28,3 +68,5 @@ if(parseInt(cartCount.textContent) > 0) {
 } else {
     cartCount.classList.add("hide");
 }
+
+checkCookie()
