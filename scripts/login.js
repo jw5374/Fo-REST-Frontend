c4e02249c90f81a1c0c2ed95136ec8f34a6f0e2e
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-07-22 20:30:30 -0400

    adding user login

--- scripts/login.js
+++ scripts/login.js
@@ -1,19 +1,29 @@
 const loginForm = document.getElementById("user-form")
 
+
 loginForm.addEventListener("submit", async (e) => {
     e.preventDefault()
     let hashedWord = hex_sha256(loginForm.passinput.value)
-    let formData = new FormData();
-    formData.set("username", loginForm.usernameinput.value)
-    formData.set("password", hashedWord)
 
-    fetch(fetchPath + "/login", {
+    fetch(fetchPath + "/auth/login", {
         method: "POST",
-        body: formData
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username: loginForm.usernameinput.value,
+            password: hashedWord
+        })
     })
     .then((res) => {
         return res.json()
     }).then((data) => {
+        let date = new Date();
+        date.setTime(date.getTime() + 7*24*60*60*1000)
         localStorage.setItem("forest-user", data.username)
+        console.log(`token=${data.token}; expires=${date.toUTCString()}; path=/`, data)
+        document.cookie += `token=${data.token}; expires=${date.toUTCString()}; path=/`
+        window.location.href = "index.html"
     })
 })
+
